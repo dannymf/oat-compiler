1138fb7cb0de69f0f3c92272fb1d83390db4ca76
Daniel Friedman <dannymf0@gmail.com>
2024-04-28 05:02:25 +0000

    dce

--- bin/dce.ml
+++ bin/dce.ml
@@ -1,44 +1,55 @@
-(** Dead Code Elimination  *)
+(** Dead Code Elimination *)
 open Ll
-open Datastructures
 
+open Datastructures
 
 (* expose a top-level analysis operation ------------------------------------ *)
 (* TASK: This function should optimize a block by removing dead instructions
-   - lb: a function from uids to the live-OUT set at the 
+   - lb: a function from uids to the live-OUT set at the
      corresponding program point
    - ab: the alias map flowing IN to each program point in the block
    - b: the current ll block
 
-   Note: 
-     Call instructions are never considered to be dead (they might produce
-     side effects)
+   Note:
+   Call instructions are never considered to be dead (they might produce
+   side effects)
 
-     Store instructions are not dead if the pointer written to is live _or_
-     the pointer written to may be aliased.
+   Store instructions are not dead if the pointer written to is live _or_
+   the pointer written to may be aliased.
 
-     Other instructions are dead if the value they compute is not live.
+   Other instructions are dead if the value they compute is not live.
 
    Hint: Consider using List.filter
- *)
-let dce_block (lb:uid -> Liveness.Fact.t) 
-              (ab:uid -> Alias.fact)
-              (b:Ll.block) : Ll.block =
-  failwith "Dce.dce_block unimplemented"
+*)
+let dce_block (lb : uid -> Liveness.Fact.t) (ab : uid -> Alias.fact) (b : Ll.block)
+  : Ll.block
+  =
+  let f (uid, insn) =
+    match insn with
+    | Call _ -> true
+    | Store (_, _, op2) ->
+      (match op2 with
+       | Id id_op ->
+         UidS.mem id_op (lb uid)
+         || UidM.find_opt id_op (ab uid) = Some Alias.SymPtr.MayAlias
+       | _ -> failwith "op not id")
+    | _ -> UidS.mem uid (lb uid)
+  in
+  let insns' = List.filter f b.insns in
+  { insns = insns'; term = b.term }
+;;
 
-let run (lg:Liveness.Graph.t) (ag:Alias.Graph.t) (cfg:Cfg.t) : Cfg.t =
-
-  LblS.fold (fun l cfg ->
-    let b = Cfg.block cfg l in
-
-    (* compute liveness at each program point for the block *)
-    let lb = Liveness.Graph.uid_out lg l in
-
-    (* compute aliases at each program point for the block *)
-    let ab = Alias.Graph.uid_in ag l in 
-
-    (* compute optimized block *)
-    let b' = dce_block lb ab b in
-    Cfg.add_block l b' cfg
-  ) (Cfg.nodes cfg) cfg
-
+let run (lg : Liveness.Graph.t) (ag : Alias.Graph.t) (cfg : Cfg.t) : Cfg.t =
+  LblS.fold
+    (fun l cfg ->
+      let b = Cfg.block cfg l in
+      (* compute liveness at each program point for the block *)
+      let lb = Liveness.Graph.uid_out lg l in
+      (* compute aliases at each program point for the block *)
+      let ab = Alias.Graph.uid_in ag l in
+      (* compute optimized block *)
+      let b' = dce_block lb ab b in
+      Cfg.add_block l b' cfg)
+    (Cfg.nodes cfg)
+    cfg
+;;
